cfe7a600b52184cefae1e6967c5738bde82dfa0e
Felix Pujols <pujols.fr@gmail.com>
2020-12-01 11:59:13 -0400

    React native / using expo

---
+++ app/screens/Restaurant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function Restaurants() {
+    return (
+        <View>
+            <Text>Restaurant....</Text> 
+        </View>
+    )
+}
