ae277ac063e5ace596f430f9778d3e7b3b81f470
Felix Pujols <pujols.fr@gmail.com>
2020-12-01 12:37:17 -0400

    React navigation installed / new views added

--- app/screens/Account.js
+++ app/screens/Account.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 
-export default function Account() {
+export default function Account()                                                                                      () {
     return (
         <View>
             <Text>Account....</Text> 
--- app/screens/Restaurant.js
+++ app/screens/Restaurant.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function Restaurants() {
     return (
         <View>
-            <Text>Restaurant....</Text> 
+            <Text>Restaurants....</Text> 
         </View>
     )
 }
