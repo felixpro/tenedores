551053fa491a3d0ad9dde09d25d7ee1029b9ef3c
Felix Pujols <pujols.fr@gmail.com>
2020-12-12 13:30:00 -0400

    React Elements installed / icons added to Nav

--- app/Navigations/Navigation.js
+++ app/Navigations/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  { NavigationContainer } from "@react-navigation/native";
 import  { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import  { Icon } from "react-native-elements";
 
 import RestaurantsStack from './RestaurantsStack';
 import FavoritesStack from './FavoritesStack';
@@ -15,7 +16,16 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+              initialRouterName="restaurants"
+              tabBarOptions={{
+                inactiveTintColor: "#646464",
+                activeTintColor: "#00a680",
+              }}
+              screenOptions={({route}) => ({
+                tabBarIcon: ({color}) => screenOptions(route, color)
+              })}
+              >
                 <Tab.Screen
                 name="restaurants"
                 component={RestaurantsStack}
@@ -45,4 +55,32 @@ export default function Navigation() {
             </Tab.Navigator>
         </NavigationContainer>
     )
+}
+
+function screenOptions(route, color){
+let iconName;
+
+switch (route.name) {
+  case "restaurants":
+    iconName = "compass-outline"
+    break;
+  case "favorites":
+    iconName = "heart-outline"
+    break;
+  case "Top-restaurants":
+    iconName = "star-outline"
+    break;
+    case "search":
+      iconName = "magnify"
+      break;
+      case "account":
+        iconName = "home-outline"
+        break;
+  default:
+  break;
+}
+
+return (
+  <Icon type="material-community" name={iconName} size={22} color={color} />
+)
 }
